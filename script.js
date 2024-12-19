ea02e58348a5952d958f5239a4a6062968a68602
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-19 19:26:17 +0400

    finished calculator project

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 
-
 const operate =  (n1, n2, operator) => {
-		if (operator.length == 0) {
-			return (0);
-		};
-		const operations = {
-			"+": n1 + n2,
-			"-": n1 - n2,
-			"x": n1 * n2,
-			"÷": n1 / n2,
-			"%": n1 % n2,
+	let jab = "no good";
+	if (operator.length == 0) {
+		return (0);
+	};
+	// evaluates multiplication to prevent the program from 
+	// crashing if the second operand is 0
+	function multiply (n1, n2) {
+		if (Number(n2) == 0) {
+			topNumber = 0; lowerNumber = 0; currentOperator = "";
+			return jab;
+		} else {
+			return n1 / n2;
 		}
-		let ans = operations[operator]
-		console.log(typeof ans);
-		if (typeof ans == "number") {
-			if (Number.isInteger(ans)) {
-				console.log(ans);
-				return ans;
-			} else {
-				console.log(ans.toFixed(2));
-				return ans.toFixed(2);
-			}
+	}
+	const operations = {
+		"+": n1 + n2,
+		"-": n1 - n2,
+		"x": n1 * n2,
+		"%": n1 / n2,
+		"÷": multiply(n1, n2), 
+	}
+	let ans = operations[operator]
+	if (ans == jab) {
+		return ans;
+	} else if (typeof ans == "number" && !isNaN(ans)) {
+		if (Number.isInteger(ans)) {
+			return ans;
+		} else {
+			return ans.toFixed(2);
 		}
+	}
 }
 
 
@@ -31,25 +40,33 @@ let lowerNumber = 0;
 let currentOperator = "";
 
 
+
 let btnContainerEl = document.querySelector("#btnContainer");
 let displayEl = document.querySelector("#display");
 let answerDisplay = document.querySelector("#answerDisplay");
 
 
+
 btnContainerEl.addEventListener("click", (e) => {
 
-
+	// for when = is clicked
 	if (e.target.textContent == "=") {
 			displayEl.textContent = 0;
 			answerDisplay.textContent = operate(Number(topNumber), 
 						Number(lowerNumber), currentOperator);
+			console.log(operate(Number(topNumber), 
+						Number(lowerNumber), currentOperator));
 			lowerNumber = 0; topNumber = 0; currentOperator = "";
 
+
+	// for when AC is clicked
 	} else if (e.target.textContent == "AC") {
 		lowerNumber = 0; topNumber = 0; currentOperator = "";
-		display.textContent = 0; answerDisplay.textContent = 0;
+		display.textContent = 0; answerDisplay.textContent = "";
 
 
+	
+	// for when a number is clicked
 	} else if (Number.isInteger(Number(e.target.textContent))) {
 		if (displayEl.textContent == 0) {
 			displayEl.textContent = e.target.textContent;
@@ -61,11 +78,18 @@ btnContainerEl.addEventListener("click", (e) => {
 		}
 
 
+	// for when an operator is clicked
 	} else if (e.target.textContent.length > 0) {
-		currentOperator = e.target.textContent;
-		if (topNumber == 0) {
+		if (currentOperator.length > 0 && 
+		topNumber > 0 && lowerNumber > 0) {
+			topNumber = operate(Number(lowerNumber),
+				 Number(topNumber), currentOperator);
+			currentOperator = e.target.textContent;
+		} else if (topNumber == 0) {
+			currentOperator = e.target.textContent;
 			topNumber = lowerNumber;
 		} else {	
+			currentOperator = e.target.textContent;
 			topNumber = operate(Number(lowerNumber),
 						 Number(topNumber), currentOperator);
 		}
